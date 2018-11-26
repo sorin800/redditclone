d459fd3d413b6662ba7c102fba0b990b2508a6a0
sorin800 <sorintruica@gmail.com>
2018-11-27 01:38:18 +0200

    Initial commit

---
+++ src/test/java/com/tsv/reddit/RedditcloneApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tsv.reddit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedditcloneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
